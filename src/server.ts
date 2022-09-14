bd5e858aebb6947a8a81afa2e81c4334cc22767b
Jorge Sant Anna <jorgeluizags@gmail.com>
2022-09-13 22:49:19 -0300

    p

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,32 @@ import express from 'express'
 
 const app = express()
 
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
+
 app.get('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
+
+    return response.json([
+        { id:1, name: 'Anúncio 1' },
+        { id:2, name: 'Anúncio 2' },
+        { id:3, name: 'Anúncio 3' },
+        { id:4, name: 'Anúncio 4' },
+    ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    // const gameId = request.params.id;
+
     return response.json([
         { id:1, name: 'Anúncio 1' },
         { id:2, name: 'Anúncio 2' },
